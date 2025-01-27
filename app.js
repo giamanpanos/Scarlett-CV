b5eb6139dda4e2cac895b58e0f11c6dc8619963d
giamanpanos <90336670+giamanpanos@users.noreply.github.com>
2025-01-27 23:41:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hamburgerContainer = document.querySelector("#header__main-nav");
 const hamburger = document.querySelector(".header__main-nav--hamburger");
 const links = document.querySelectorAll(".header__main-nav--links li");
 
+const form = document.querySelector(".contact__form");
 const submit = document.querySelector("#contact_submit");
 
 hamburger.addEventListener("click", () => {
@@ -14,6 +15,7 @@ hamburger.addEventListener("click", () => {
 submit.addEventListener("click", (e) => {
     e.preventDefault()
     setTimeout(() => {
+    form.reset()
     alert("Your message has been sent successfully. I will get back to you asap.")
     }, 1000)
 })
