665025ac5b6ded98ed45b31739cb1621a4697d58
giamanpanos <90336670+giamanpanos@users.noreply.github.com>
2025-01-27 23:07:41 +0200

    contact form submission function

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const hamburgerContainer = document.querySelector("#header__main-nav");
 const hamburger = document.querySelector(".header__main-nav--hamburger");
 const links = document.querySelectorAll(".header__main-nav--links li");
 
+const name = document.querySelector("#contact_name")
+const email = document.querySelector("#contact_email")
+const subject = document.querySelector("#contact_subject")
+const message = document.querySelector("#contact_message")
+const submit = document.querySelector("#contact_submit")
+
 hamburger.addEventListener("click", () => {
     hamburgerContainer.classList.toggle("clicked");
     links.forEach((link) => {
@@ -9,3 +15,9 @@ hamburger.addEventListener("click", () => {
     });
 });
 
+function submitForm(e) {
+    e.preventDefault()
+    setTimeOut(() => {
+    alert("Your message has been sent successfully. I will get back to you asap.")
+    })
+}
